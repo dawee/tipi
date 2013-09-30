f1f669ca867e5a09cb71a66be7d6864eeb05bcef
David <dawi@zergling.local>
2013-10-01 00:26:54 +0200

    install underscore in tmp directory

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var npm = require('npm');
+var os = require('os');
+var path = require('path');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
 exports.install = function (options) {
+    var prefix = path.join(os.tmpdir(), 'tipi');
+
+    try {
+        fs.mkdirSync(tmpdir)
+    } catch(e) {
+    }
+
+    npm.load({prefix: prefix, loglevel: 'error'}, function () {
+        npm.commands.install(['underscore'], function (error, data) {
+            rimraf.sync(prefix);
+        });
+
+    });
 
 };
