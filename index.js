33159feb0aa62dd2c5e4677d0bd072486f7dfa5f
David <dawi@zergling.local>
2013-10-01 00:27:53 +0200

    install underscore in tmp directory

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var os = require('os');
 var path = require('path');
 var fs = require('fs');
 var rimraf = require('rimraf');
+var tetanize = require('tetanize');
 
 exports.install = function (options) {
     var prefix = path.join(os.tmpdir(), 'tipi');
@@ -14,6 +15,7 @@ exports.install = function (options) {
 
     npm.load({prefix: prefix, loglevel: 'error'}, function () {
         npm.commands.install(['underscore'], function (error, data) {
+            tetanize.build({path: path.join(prefix, 'underscore'), out: './underscore.js'})
             rimraf.sync(prefix);
         });
 
